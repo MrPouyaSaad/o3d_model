cff60508558d9c954db655ac1171275661d7e3e3
Pouya Sadeghzadeh <122441650+MrPouyaSaad@users.noreply.github.com>
2023-10-10 19:52:23 +0330

    Make Bottom Navigation

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
 
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int index = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,6 +18,24 @@ class MainScreen extends StatelessWidget {
         title: const Text('3D Model'),
         centerTitle: true,
         elevation: 0.0,
+      ),
+      body: Container(),
+      bottomNavigationBar: BottomNavigationBar(
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        currentIndex: index,
+        onTap: (value) {
+          setState(() {
+            index = value;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.timer_outlined), label: 'Timer'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.analytics_outlined), label: 'Analytics'),
+        ],
       ),
     );
   }
